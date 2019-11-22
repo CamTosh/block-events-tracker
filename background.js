3788096c981e25c7a7f2f9364fcd6da08f905196
CamTosh <tochecamille@gmail.com>
2019-11-22 14:45:36 +0100

    fix

--- background.js
+++ background.js
@@ -1,34 +1,8 @@
-let tracker = "*://canvas.supinfo.com/api/v1/courses/*/quizzes/*/submissions/*/events"
-
+let tracker = "https://canvas.supinfo.com/api/v1/courses/*/quizzes/*/submissions/*/events"
 window[typeof chrome !== "undefined" ? 'chrome' : 'browser'].webRequest.onBeforeRequest.addListener(
 	(request) => {
 		console.log('Block:' + request.url)
-
-		if (request.method == "POST") {
-			let body = request.requestBody
-
-			if (body.raw) {
-				let postedString = decodeURIComponent(
-					String.fromCharCode.apply(null, new Uint8Array(body.raw[0].bytes))
-				);
-
-				try {
-					let data = JSON.parse(postedString)
-					data.quiz_submission_events.forEach((evt) => console.log('Event Type: ' + evt.event_type))
-
-					let blockedEvents = localStorage.getItem('blocked_events') || 0
-					
-					//localStorage.setItem('blocked_events', blockedEvents += data.quiz_submission_events.length)
-					localStorage.setItem('blocked_events', blockedEvents += 1)
-
-				} catch(e) {
-					console.log("Failed to parse posted string")
-					console.log(postedString)
-				}
-			}
-		}
-
-		return { cancel: request.url.indexOf(tracker) != -1 };
+		return { cancel: true };
 	},
 	{ urls: [tracker] },
 	["blocking", "requestBody"]
