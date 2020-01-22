3d38850cf9f01ee082180636da75fc50513c4120
Toche Camille <tochecamille@gmail.com>
2020-01-22 09:25:25 +0100

    Remove failed events saved on localstorage

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
 let tracker = "https://canvas.supinfo.com/api/v1/courses/*/quizzes/*/submissions/*/events"
 window[typeof chrome !== "undefined" ? 'chrome' : 'browser'].webRequest.onBeforeRequest.addListener(
 	(request) => {
-		console.log('Block:' + request.url)
+		console.log('Block:' + request.url);
+		console.log('Remove localstorage qla_events');
+		localStorage.removeItem('qla_events');
 		return { cancel: true };
 	},
 	{ urls: [tracker] },
